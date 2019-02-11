919a3ca08d847793ee90b69a67fef9f9e547688f
Brad Cook <cookbrad@oregonstate.edu>
2019-02-11 13:30:50 -0800

    reformat json

--- app.js
+++ app.js
@@ -27,14 +27,19 @@ app.get('/', function(req, res) {
             if (err) console.log(err); 
             else{
                 let album = [];
-                var test =  data.Contents.map(obj => {
-                   return obj.Key.split('/')
+                var artists =  data.Contents.map(obj => {
+                   var object = obj.Key.split('/')
+                   var artist = object[1]
+                   var album = object[2]
+                   var song = object[3]
+                   return {
+                       "artist" : artist,
+                       "album" : album,
+                       "song" : song
+                   }
+
                 });
-                let artists = new Set(test.map(path => (path[0])));
-                artists.forEach(artist => {
-                    album.push({ artist })
-                  });
-                res.send(test);
+                res.send(artists);
             }
         });    
     };// successful response

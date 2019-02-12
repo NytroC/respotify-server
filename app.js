716449335ab1a435816aa826a17eacf35c09e3a5
Brad Cook <cookbrad@oregonstate.edu>
2019-02-11 18:08:28 -0800

    changes

--- app.js
+++ app.js
@@ -7,19 +7,7 @@ const port = 3000
 
 
 app.get('/', function(req, res) {
-    var sts = new AWS.STS();
-    var params = {
-        DurationSeconds: 3600, 
-        RoleArn: "arn:aws:iam::170621239995:role/s3admin", 
-        RoleSessionName: "song-session"
-    };
-    sts.assumeRole(params, function (err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else{
-        console.log(data);
-        AWS.config.credentials = sts.credentialsFrom(data);
-        console.log("before")
-        console.log(AWS.config.credentials);
+    
         var songBucket = new AWS.S3({
            apiVersion: '2006-03-01',
            params: {Bucket: "do-not-kick"}
@@ -45,7 +33,5 @@ app.get('/', function(req, res) {
                 res.send(artists);
             }
         });    
-    };// successful response
-    });
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
